5f5b54db06e2f5ee1ef8f935ce85f0de07e3c88b
Arkin Modi <arkinmodi@gmail.com>
2020-10-16 18:10:52 -0400

    Added functionality to create list from input text

--- src/App.js
+++ src/App.js
@@ -2,19 +2,60 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      nextID: 0,
+      inputItem: "",
+      inputCategory: "",
+    };
+  }
+
+  // Submit button function
+  submit() {
+    this.setState({
+      nextID: this.state.nextID + 1,
+      inputItem: "",
+      inputCategory: "",
+      items: [
+        ...this.state.items,
+        {
+          item: this.state.inputItem,
+          category: this.state.inputCategory,
+          id: this.state.nextID + 1,
+        },
+      ],
+    });
+  }
+
   render() {
     return (
       <div>
-        <input type="text" />
-        <input type="text" />
-        <button>Submit</button>
+        {/* Category Input Box */}
+        <input
+          type="text"
+          onChange={(event) =>
+            this.setState({ inputCategory: event.target.value })
+          }
+          value={this.state.inputCategory}
+        />
+
+        {/* Item Input Box */}
+        <input
+          type="text"
+          onChange={(event) => this.setState({ inputItem: event.target.value })}
+          value={this.state.inputItem}
+        />
+
+        {/* Submit Button */}
+        <button onClick={this.submit.bind(this)}>Submit</button>
+
+        {/* List of Items */}
         <ul>
-          <li>
-            Item 1
-            <ul>
-              <li>Sub Item 1</li>
-            </ul>
-          </li>
+          {this.state.items.map(({ item, id }) => (
+            <li key={id}>{item}</li>
+          ))}
         </ul>
       </div>
     );
